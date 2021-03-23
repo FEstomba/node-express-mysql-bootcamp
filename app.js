f5dac099cab216eb924e8d9509a8db92248fd1d6
FEstomba <florenciaestomba91@gmail.com>
2021-03-22 22:06:51 -0300

    creacion de clientes y rutas

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ const connection = mysql.createConnection({
 });
 
 //Route
+
 //declaro un servicio que provee el servidor
 //le digo a express que es un servicio de tipo GET a la ruta "/" 
 //cuando este servicio recibe un request retorna una cadena "Welcome to my API"
@@ -30,41 +31,87 @@ app.get ('/', (req, res) =>{
 });
 
 //All customers
+
 //declaro un servicio que provee el servidor
 //le digo a express que es un servicio de tipo GET a la ruta "/customers" 
-//cuando este servicio recibe un request retorna una cadena "List of customers"
+//cuando este servicio recibe un request se posiciona sobre el listado de customers si los hay
+//con la variable de la dependencia mysql (connection) podemos acceder a un metodo .query al que se le pasara primero el query(sql) y la funcion de callback
+//la funcion nos va a devolver error o el resultado de la query, si existe un error lanzara error, si el resultado es mayor a cero significa que se han podido recuperar resultados 
+//en caso de que no haya resultados lanzara un msj con que no se encontraron resultados
 app.get('/customers', (req,res) =>{
-     
+     const sql ='SELECT * FROM customers';
 
+     connection.query(sql,( error, results) =>{
+         if (error) throw error;
+         if (results.length > 0){
+             res.json(results)
+         }else{
+             res.send('Not result');
+         }
+     });
 
-
-
-    res.send('List of customers')
 });
-//cuando este servicio recibe un request retorna una cadena "Get customer by ID"
+//cuando este servicio recibe un request por id , recibe parametros 
 app.get('/customers/:id', (req,res) =>{
-    res.send('Get customer by ID')
+    const {id} = req.params
+    const sql = `SELECT * FROM customers WHERE id = ${id}`;
+    connection.query(sql,( error, result) =>{
+        if (error) throw error;
+
+        if (results.length > 0){
+            res.json(result)
+        }else{
+            res.send('Not result');
+        }
+    });
+
 });
+
 //le digo a express que es un servicio de tipo POST a la ruta "/add" 
-//cuando este servicio recibe un request retorna una cadena "New customer"
+//variable qsl que es igual a insertar un nuevo customers colocando la informacion del objeto
 app.post('/add', (req,res) =>{
-    res.send('New customer')
+    const sql = 'INSERT INTO customers SET ?';
+//creamos un objeto con la informacion que nos viene desde el frontend
+    const customerObj = {
+        name: req.body.name,
+        city: req.body.city
+    };
+
+    connection.query(sql, customerObj, error => {
+        if (error) throw error;
+        res.send('Customer created');
+    })
 });
+
 //le digo a express que es un servicio de tipo PUT a la ruta "/update/:id" 
 //cuando este servicio recibe un request retorna una cadena "Update customer"
 app.put('/update/:id', (req,res) =>{
-    res.send('Update customer')
-});
+    const {id} = req.params;
+    const {name,city} = req.body;
+    const sql = `UPDATE customers SET name = '${name}', city = '${city}' WHERE id =${id}`;
+
+    connection.query(sql, error => {
+        if (error) throw error;
+        res.send('Customer updated');
+
+})});
+
 //le digo a express que es un servicio de tipo DELETE a la ruta "/delete/:id" 
 //cuando este servicio recibe un request retorna una cadena "Delete customer"
 app.delete('/delete/:id', (req,res) =>{
-    res.send('Delete customer')
-});
+    const {id} = req.params;
+    const sql = `DELETE FROM customers WHERE id = ${id}`;
+
+    connection.query(sql, error => {
+        if (error) throw error;
+        res.send('Delete customer')
+})});
 
 //Crea una conexion con la base de datos 
 connection.connect(error =>{
     if (error) throw error;
     console.log('database server running!')
 });
+
 //Inicio el servidor en el puerto configurado
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- baseDeDatos.sql
+++ baseDeDatos.sql
@@ -2,7 +2,9 @@ create schema node20_mysql;
 use node20_mysql;
 
 create table  customers(
-    id int auto_increment not null,
+    id bigint unsigned not null auto_increment,,
     name varchar(64) not null,
-    city varchar(64) defaul null,
-)
+    city varchar(64) default null,
+    primary key (id)
+);
+insert into customers(name,city) values("Gabito","San Mikel");
